7e10f5289317f57a6071a85db39abcdb50d4bf31
Sasaki kaisei <k.sasaki.sys23@morijyobi.ac.jp>
2023-11-09 15:08:16 +0900

    2人分のbmi

--- Main.java
+++ Main.java
@@ -4,11 +4,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("身長,体重の順に入力をお願いします");
         double h = sc.nextDouble();
+        double hs = sc.nextDouble();
         double w = sc.nextDouble();
+        double ws = sc.nextDouble();
         double h_b = h / 100;
+        double h_bs = hs / 100;
         double b = w / (h_b * h_b);
+        double bs = ws / (h_bs * h_bs);
         String s = String.format("%.2f", b);
+        String ss = String.format("%.2f", bs);
         double b2 = Double.parseDouble(s);
-        System.out.println("あなたのBMI数値は" + b2 + "になります。");
+        double b2s = Double.parseDouble(ss);
+        System.out.println("一番さんのBMI数値は" + b2 + "になります。");
+        System.out.println("二番さんのBMI数値は" + b2s + "になります。");
     }
 }
