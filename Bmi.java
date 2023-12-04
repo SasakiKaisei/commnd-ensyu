76333b3d8bda3ea71ac8341c34a79866a0fde4a5
Sasaki kaisei <k.sasaki.sys23@morijyobi.ac.jp>
2023-12-04 13:10:49 +0900

    Bmi.javaとREADME.mdの修正。
    Bmi.javaの16と17行目を削除。
    README.mdの全角スペースから半角スペースに修正。

--- Bmi.java
+++ Bmi.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-public class Main {
+
+public class bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("身長,体重の順に入力をお願いします");
         double h = sc.nextDouble();
-        double hs = sc.nextDouble();
         double w = sc.nextDouble();
+        double hs = sc.nextDouble();
         double ws = sc.nextDouble();
         double h_b = h / 100;
         double h_bs = hs / 100;
@@ -13,9 +14,7 @@ public class Main {
         double bs = ws / (h_bs * h_bs);
         String s = String.format("%.2f", b);
         String ss = String.format("%.2f", bs);
-        double b2 = Double.parseDouble(s);
-        double b2s = Double.parseDouble(ss);
-        System.out.println("一番さんのBMI数値は" + b2 + "になります。");
-        System.out.println("二番さんのBMI数値は" + b2s + "になります。");
+        System.out.println("一番さんのBMI数値は" + s + "になります。");
+        System.out.println("二番さんのBMI数値は" + ss + "になります。");
     }
 }
